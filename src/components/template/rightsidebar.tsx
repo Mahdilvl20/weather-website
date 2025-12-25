e50271953ce54fd76723c808e387aa97fcba158f
Mahdi <mahdiahmadibidani@gmail.com>
2025-12-25 19:27:21 +0330

    add snowfall

--- src/components/template/rightsidebar.tsx
+++ src/components/template/rightsidebar.tsx
@@ -4,11 +4,13 @@ import sun from '@/assets/sun.svg';
 import cloudy from '@/assets/cloudy.svg';
 import cloudysunyrainny from '@/assets/cloudysunyrainny.svg';
 import rain from '@/assets/rain.svg';
+import Snowfall from 'react-snowfall';
 
 
 export default function Rightsidebar(){
  return (
      <div className={"flex flex-col pl-2"}>
+         <Snowfall color="#dee4fd" wind={[2,1]} />
         <div className={"flex items-start ml-5 p-2"}>
             <Label className={"text-white text-2xl underline decoration-white decoration-2 underline-offset-4 [text-decoration-style:wavy] tracking-widest pt-1"}>Week</Label>
         </div>
