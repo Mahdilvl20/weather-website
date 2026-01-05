791f534e2c8520436fba265a21ad848ec99d870d
Mahdi <mahdiahmadibidani@gmail.com>
2026-01-05 20:47:27 +0330

    get info in APP.tsx and get that in leftsidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import './App.css'
 import {Card} from "@/components/ui/card.tsx";
 import Leftsidebar from "./components/template/leftsidebar.tsx";
 import Rightsidebar from "@/components/template/rightsidebar.tsx";
+import {useState} from "react";
 function App() {
+    const [whatherData, setWhatherData] = useState<any>(null);
+    const handleCitySelectFromSidebar=async (cityInfo:{name:string,lat:number,lon:number})=> {
+        console.log("city: ",cityInfo);
+        try {
+            const res=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${cityInfo.lat}&longitude=${cityInfo.lon}&current=temperature_2m,relative_humidity_2m,is_day,precipitation,rain,weather_code,pressure_msl,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=auto`)
+            const data=await res.json()
+
+            setWhatherData(data);
+            console.log("data:",data);
+        }catch(err){
+            console.log("error to receive: ",err);
+        }
+    }
 
   return (
       <div className="flex flex-col md:flex-row gap-3 p-1">
 
       <Card className="bg-[#303236] border-[#303236] md:w-170 w-full flex items-center justify-center">
-          <Leftsidebar />
+          <Leftsidebar onCitySelected={handleCitySelectFromSidebar} weatherData={whatherData} />
       </Card>
           <Card className="bg-[#303236] border-[#303236] md:w-full w-full ">
                 <Rightsidebar/>
--- src/components/template/leftsidebar.tsx
+++ src/components/template/leftsidebar.tsx
@@ -30,6 +30,24 @@ export default function Leftsidebar({onCitySelected,weatherData}: LeftsideProps)
         const cityDetail=Array.isArray(cities)?cities:Object.values(cities|| {});
         setCityDetailList(cityDetail);
     }
+    const getDayName = (dateString:any) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', { weekday: 'long' });
+    };
+    const getWeatherDescription = (code: number): string => {
+        switch (code) {
+            case 0: return "Clear Sky";
+            case 1: return "Mainly Clear";
+            case 2: return "Partly Cloudy";
+            case 3: return "Overcast";
+            case 45: case 48: return "Foggy";
+            case 51: case 53: case 55: return "Drizzle";
+            case 61: case 63: case 65: return "Rainy";
+            case 71: case 73: case 75: return "Snowy";
+            case 95: case 96: case 99: return "Thunderstorm";
+            default: return "Unknown";
+        }
+    };
     return (
         <div className="flex flex-col justify-center items-center w-full">
             <div className="relative w-90">
@@ -47,7 +65,6 @@ export default function Leftsidebar({onCitySelected,weatherData}: LeftsideProps)
                             <CommandEmpty>City not found.</CommandEmpty>
                             <CommandGroup>
                                 {cityDitailList?.map((city,index)=>(
-                                    console.log(city),
                                     <CommandItem
                                         key={index}
                                         onSelect={()=>{
@@ -73,39 +90,41 @@ export default function Leftsidebar({onCitySelected,weatherData}: LeftsideProps)
                 <img src={cloudysunyrainny} alt={"cloudysunyrainny"} className={"w-70 mt-10"}/>
             </div>
             <div className={"flex justify-center"}>
-                <Label className={"text-white text-7xl font-bold mt-5"}>{weatherData?.current?.temperature_2m || 30}
+                <Label className={"text-white text-7xl font-bold mt-5"}>{weatherData?.current?.temperature_2m || ""}
                     <sup className="text-5xl font-bold">°C</sup>
                 </Label>
             </div>
             <div className={"flex justify-center pt-15 gap-30"}>
-                <Label className={"text-white text-2xl "}>isfahan</Label>
-                <Label className={"text-white text-2xl "}>Monday</Label>
+                <Label className={"text-white text-2xl "}>{value}</Label>
+                <Label className={"text-white text-2xl "}>{weatherData?.current?.time?getDayName(weatherData.current.time):'empty'}</Label>
             </div>
             <Separator className={'m-5 mx-auto'} style={{width:'80%'}} />
             <div className={"flex flex-col gap-2 items-start w-[80%] mt-10"}>
                 <div className={"flex gap-2 w-full"}>
                     <img src={cloudrainsmall} alt={"cloudrainsmall"}  className="w-4 h-4 shrink-0"/>
-                    <Label className={"text-white"}>Cloudy Rainny</Label>
+                    <Label className={"text-white"}>{weatherData?.current?.weather_code?getWeatherDescription(weatherData.current.weather_code):"loading..."}</Label>
                 </div>
                 <div className={"flex gap-2"}>
                     <img src={mintemp} alt={"mintemp"} className="w-4 h-4 shrink-0"/>
                     <Label className={"text-white"}>Min temperature</Label>
+                    <Label className={"text-white"}>{weatherData?.daily?.temperature_2m_min[0]}</Label>
                 </div>
                 <div className={"flex gap-2"}>
                     <img src={maxtemp} alt={"maxtemp"} className="w-4 h-4 shrink-0"/>
                     <Label className={"text-white"}>Max temperature</Label>
+                    <Label className={"text-white"}>{weatherData?.daily?.temperature_2m_max[0]}</Label>
                 </div>
             </div>
             <div className={"w-fit flex bg-[#252222] rounded-md mt-15 shadow-lg shadow-black/60"}>
                 <div className={"flex justify-center m-2"}>
                     <img src={water} alt={"water"} className="w-10 h-10 shrink-0"/>
                     <div className={"flex flex-col pl-2"}>
-                        <Label className={"text-white pt-1"}>83%</Label>
+                        <Label className={"text-white pt-1"}>{weatherData?.current?.relative_humidity_2m}%</Label>
                         <Label className={"text-white pt-1"}>Humidity</Label>
                     </div>
                     <img src={stroke} alt={"stroke"} className={"w-10 h-10 shrink-0 ml-2"}/>
                     <div className={"flex flex-col pl-2"}>
-                        <Label className={"text-white pt-1"}>6km/h</Label>
+                        <Label className={"text-white pt-1"}>{weatherData?.current?.wind_speed_10m}km/h</Label>
                         <Label className={"text-white pt-1"}>Wind Speed</Label>
                     </div>
                 </div>
